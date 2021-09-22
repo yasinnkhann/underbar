1320c663266a95391b2539b1be27753e54fceb0b
Yasin Khan <yasinkhan500@gmail.com>
2021-09-22 14:43:42 -0700

    made it to uniq

--- src/underbar.js
+++ src/underbar.js
@@ -50,6 +50,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection .length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    }
+    if (!Array.isArray(collection) && typeof collection === 'object') {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -71,12 +81,24 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        if (test(collection[i]) === true) {
+          result.push(collection[i]);
+        }
+      }
+    }
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(val) {
+      return test(val) === false;
+    });
   };
 
   // Produce a duplicate-free version of the array.
